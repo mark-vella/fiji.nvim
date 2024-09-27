267e8e8622ddaeb2d211cdf5bf8b1b79513a00e4
Mark Vella <me@markvella.dev>
2024-09-27 21:32:14 +0200

    chore: split colors from themery

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,42 +1,7 @@
 return {
-  {
-    'Mofiqul/vscode.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'vscode'
-      vim.o.background = 'dark'
-    end,
-  },
-
-  { 'rebelot/kanagawa.nvim' },
-
-  { 'catppuccin/nvim', name = 'catppuccin' },
-
-  { 'rose-pine/neovim', name = 'rose-pine' },
-
-  { 'sainnhe/gruvbox-material' },
-
-  { 'projekt0n/github-nvim-theme' },
-
-  { 'jthvai/lavender.nvim' },
-
-  {
-    'savq/melange-nvim',
-    config = function()
-      vim.cmd.colorscheme 'melange'
-    end,
-  },
-
-  {
-    'briones-gabriel/darcula-solid.nvim',
-    dependencies = {
-      'rktjmp/lush.nvim',
-    },
-    config = function()
-      vim.cmd.colorscheme 'darcula-solid'
-      vim.o.background = 'dark'
-      vim.o.termguicolors = true
-    end,
-  },
+  'brenoprata10/nvim-highlight-colors',
+  config = function()
+    vim.opt.termguicolors = true
+    require('nvim-highlight-colors').setup {}
+  end,
 }
---
+++ lua/custom/plugins/themes.lua
@@ -0,0 +1,24 @@
+return {
+  { 'Mofiqul/vscode.nvim' },
+
+  { 'rebelot/kanagawa.nvim' },
+
+  { 'catppuccin/nvim', name = 'catppuccin' },
+
+  { 'rose-pine/neovim', name = 'rose-pine' },
+
+  { 'sainnhe/gruvbox-material' },
+
+  { 'projekt0n/github-nvim-theme' },
+
+  { 'jthvai/lavender.nvim' },
+
+  { 'savq/melange-nvim' },
+
+  {
+    'briones-gabriel/darcula-solid.nvim',
+    dependencies = {
+      'rktjmp/lush.nvim',
+    },
+  },
+}
