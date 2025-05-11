eafd7d8c76cac3520a3120ad1f02bac7336292a9
Mark Vella <me@markvella.dev>
2025-05-11 12:33:21 +0200

    fix: broken syntax

--- init.lua
+++ init.lua
@@ -432,12 +432,6 @@ require('lazy').setup({
           },
         },
       }
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
-        },
-      }
 
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
